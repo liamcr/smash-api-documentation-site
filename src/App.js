8be8ddf7d0576b9cbe67ebb4ee17194f9e84f8c2
Jacob Wieler <jacobwieler21@gmail.com>
2020-03-01 11:41:23 -0500

    homepage functionality

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
 import React from "react";
 import "./App.css";
 import {
-  createMuiTheme,
-  ThemeProvider,
-  AppBar,
-  Toolbar,
-  makeStyles
+    createMuiTheme,
+    ThemeProvider,
+    AppBar,
+    Toolbar,
+    Container,
+    TextField,
+    makeStyles,
+    CssBaseline
 } from "@material-ui/core";
 import { HomePage } from "./pages/HomePage";
+import { dark } from "@material-ui/core/styles/createPalette";
 
 const useStyles = makeStyles(theme => ({
-  title: {
-    ...theme.typography.subtitle1,
-    fontSize: "24pt"
-  }
+    title: {
+        ...theme.typography.subtitle1,
+        fontSize: "24pt"
+    }
 }));
 
 function App() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: "#ff0000"
-      }
-    }
-  });
+    const theme = createMuiTheme({
+        palette: {
+            primary: {
+                main: "#d90004"
+            },
+            secondary: {
+                main: "#2e2c2c"
+            },
+            type: "dark"
+        }
+    });
 
-  return (
-    <ThemeProvider theme={theme}>
-      <AppBar position="sticky">
-        <Toolbar>
-          <div className={classes.title}>Smash Ultimate API</div>
-        </Toolbar>
-      </AppBar>
-      <HomePage />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AppBar position="sticky">
+                <Toolbar>
+                    <div className={classes.title}>Smash Ultimate API</div>
+                </Toolbar>
+            </AppBar>
+
+            <HomePage />
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,201 @@
 import React from "react";
+import {
+    createMuiTheme,
+    ThemeProvider,
+    AppBar,
+    Toolbar,
+    Container,
+    Box,
+    TextField,
+    makeStyles,
+    Button,
+    CircularProgress,
+    Card,
+    CardContent,
+    Typography
+} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    title: {
+        ...theme.typography.subtitle1,
+        fontSize: "24pt"
+    },
+    jsonArea: {
+        overflow: "scroll",
+        padding: "10px",
+        resize: "none",
+        width: "100%",
+        color: "whitesmoke",
+        backgroundColor: "transparent",
+        border: "none",
+        disabled: "true",
+        height: "500px"
+    },
+    container: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "auto",
+        height: "500px"
+    },
+    query: {
+        display: "inline"
+    },
+    queryCont: {
+        display: "flex",
+        alignItems: "baseline",
+        justifyContent: "center",
+        width: "fit-content",
+        margin: "auto",
+        padding: "10px"
+    },
+    body: {
+        width: "90%",
+        margin: "auto"
+    },
+    root: {
+        minWidth: 275,
+        minHeight: 275,
+        width: "100%",
+        maxHeight: 500,
+        height: 500,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "10px"
+    },
+    jsonCont: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: 275,
+        height: "fit-content",
+        maxHeight: 500
+    }
+}));
 
 export function HomePage() {
-  return <></>;
+    const classes = useStyles();
+
+    const [response, setResponse] = React.useState(false);
+    const [waiting, setWaiting] = React.useState(false);
+    const [init, setInit] = React.useState(true);
+
+    const sendQuery = () => {
+        let query = document.getElementById("queryText").value;
+        if (query.length === 0) {
+            return;
+        }
+        let sampleReq = new XMLHttpRequest();
+        let responseJSON = "";
+        let baseUrl = "https://api.smashultimate.ca/";
+        let url = baseUrl + query;
+
+        sampleReq.open("GET", url, true);
+        sampleReq.onload = function() {
+            responseJSON = JSON.parse(this.response);
+            responseJSON = JSON.stringify(responseJSON, undefined, 4);
+            setResponse(responseJSON);
+            setWaiting(false);
+        };
+        sampleReq.onreadystatechange = () => {
+            if (sampleReq.readyState === 4) {
+                //if complete
+                if (sampleReq.status === 200) {
+                    //check if "OK" (200)
+                    //success
+                } else {
+                    setWaiting(false);
+                    setResponse("404 page not found");
+                }
+            }
+        };
+        setWaiting(true);
+        setResponse(false);
+        sampleReq.send();
+    };
+
+    /*
+    let sampleReq = new XMLHttpRequest();
+    let responseJSON = "";
+
+    sampleReq.open("GET", "https://api.smashultimate.ca/moves", true);
+    sampleReq.onload = function() {
+        responseJSON = JSON.parse(this.response);
+        responseJSON = JSON.stringify(responseJSON, undefined, 4);
+        setResponse(responseJSON);
+    };
+    sampleReq.send();
+*/
+    let test = "";
+    let test2 = "";
+    if (response) {
+        test = response;
+        /*test2 = (
+            <textarea
+                className={classes.jsonArea}
+                style={{ disabled: "true" }}
+                name=""
+                id="myTextarea"
+                cols="30"
+                rows="10"
+                value={test}
+                readOnly
+                disabled={true}
+            ></textarea>
+        );
+        */
+
+        test2 = <textarea label="Result" className={classes.jsonArea} value={test}></textarea>;
+    }
+
+    let wait = "";
+    if (waiting) {
+        wait = <CircularProgress />;
+    }
+
+    if (init) {
+        let sampleReq = new XMLHttpRequest();
+        let responseJSON = "";
+
+        test = require("../defaultJSON/homepage.json");
+        /* test2 = (
+            <textarea
+                className={classes.jsonArea}
+                style={{ disabled: "true" }}
+                name=""
+                id="myTextarea"
+                cols="30"
+                rows="10"
+                value={test}
+                readOnly
+                disabled={true}
+            ></textarea>    
+        );
+        */
+
+        test2 = <textarea label="Result" className={classes.jsonArea} value={test}></textarea>;
+
+        setResponse(JSON.stringify(test, undefined, 4));
+        setInit(false);
+    }
+    return (
+        <div className={classes.body}>
+            <div className={classes.queryCont}>
+                <div className={classes.query}>https://api.smashultimate.ca/</div>
+                <TextField id="queryText" label="sampleEndpoint" variant="standard" defaultValue="moves"></TextField>
+                <Button onClick={sendQuery} style={{ marginLeft: "10px" }} variant="contained">
+                    Confirm
+                </Button>
+            </div>
+            <div className={classes.jsonCont}>
+                <Card className={classes.root}>
+                    <CardContent className={classes.root}>
+                        {test2}
+                        {wait}
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    );
 }
