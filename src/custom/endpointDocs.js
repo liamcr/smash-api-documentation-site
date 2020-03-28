601fba6c95cbf1bcf3867f7a5563c08e353507e7
lcrocket <liamcrocket@gmail.com>
2020-03-28 16:52:38 -0400

    Added stages documentation. Also changed homepage default query to '/moves?ids=1' just so there isn't an overwhelmingly huge JSON to start

--- src/custom/endpointDocs.js
+++ src/custom/endpointDocs.js
@@ -58,7 +58,6 @@ export const endpointDocs = [
         responseFormat: {
           description: "An array of character objects",
           objectName: "Character Object",
-
           responseObjectKeys: [
             {
               key: "id",
@@ -87,6 +86,97 @@ export const endpointDocs = [
               valueDescription:
                 "Object containing all character attribute info",
               linkTo: "character-attributes"
+            }
+          ]
+        }
+      }
+    ]
+  },
+  {
+    endpointName: "Stages",
+    id: "stages-endpoint",
+    usages: [
+      {
+        path: "/stages",
+        url: "https://api.smashultimate.ca/stages",
+        description:
+          "When invoked without the 'ids' parameter, will return an array with every stage.",
+        queryParamKeys: [],
+        responseFormat: {
+          description: "An array of stage objects",
+          objectName: "Stage Object",
+          responseObjectKeys: [
+            {
+              key: "id",
+              valueType: "Number",
+              valueDescription: "The unique id of the stage"
+            },
+            {
+              key: "introducedIn",
+              valueType: "String",
+              valueDescription:
+                "The name of the game this stage was introduced in"
+            },
+            {
+              key: "universe",
+              valueType: "String",
+              valueDescription: "The video game series the stage is from"
+            },
+            {
+              key: "isStarterStage",
+              valueType: "Boolean",
+              valueDescription:
+                "True if the stage is unlocked from the start of the game, without DLC"
+            },
+            {
+              key: "stageName",
+              valueType: "String",
+              valueDescription: "The english name of the stage"
+            }
+          ]
+        }
+      },
+      {
+        path: "/stages?ids={ids}",
+        url: "https://api.smashultimate.ca/stages?ids={ids}",
+        description:
+          "When passed a comma-separated list of ids, returns an array of the corresponding stages.",
+        queryParamKeys: [
+          {
+            parameter: "ids",
+            value: "A comma-separated list of stage ids"
+          }
+        ],
+        responseFormat: {
+          description: "An array of stage objects",
+          objectName: "Stage Object",
+          responseObjectKeys: [
+            {
+              key: "id",
+              valueType: "Number",
+              valueDescription: "The unique id of the stage"
+            },
+            {
+              key: "introducedIn",
+              valueType: "String",
+              valueDescription:
+                "The name of the game this stage was introduced in"
+            },
+            {
+              key: "universe",
+              valueType: "String",
+              valueDescription: "The video game series the stage is from"
+            },
+            {
+              key: "isStarterStage",
+              valueType: "Boolean",
+              valueDescription:
+                "True if the stage is unlocked from the start of the game, without DLC"
+            },
+            {
+              key: "stageName",
+              valueType: "String",
+              valueDescription: "The english name of the stage"
             }
           ]
         }
